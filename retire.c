996303fe899c76cdb7b47305f36e6ac9b20eb88c
hifza <hifzanazir456@gmail.com>
2023-10-22 22:33:58 +0500

    author and program info , predirectives

---
+++ retire.c
@@ -0,0 +1,10 @@
+/**
+ * Author: Hifza Nazir 
+ * Date: 2023/10/22
+ * Email: hifzanazir456@gmail.com
+ * 
+ * The prgram about 401(k) plan is a account for people to save money for retirement
+*/
+#include<stdio.h>
+#include<math.h>
+#include<stdlib.h>
